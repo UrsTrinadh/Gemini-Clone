04a69359c48cc197a2202b920158f2d8eeade075
Trinadh <166199109+UrsTrinadh@users.noreply.github.com>
2025-03-22 15:40:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,67 @@
+**Trinadh**
+
+# **Google Gemini AI Clone**
+
+Welcome to the **Google Gemini AI Clone** repository! This project is a clone of Google Gemini AI, built using **React.js** and integrated with the **Google Generative AI API**. The application is designed to be fully responsive, ensuring a seamless experience across all devices.
+
+## **Features**
+
+- **Responsive Design:** The application is fully responsive, providing an optimal user experience on desktops, tablets, and mobile devices.
+- **Google Generative AI Integration:** Leverages the power of Google Generative AI API for generating intelligent responses and interactions.
+- **Interactive UI:** Includes interactive elements such as cards with random questions *(coming soon)*, video, and audio features *(coming soon)*.
+
+## **Getting Started**
+
+1. **Clone the repository:**
+   ```bash
+   `git clone https://github.com/UrsTrinadh/Gemini-Clone.git`
+
+2. **Install dependencies**
+
+    `npm install`
+
+3. **Start the development server**
+
+    `npm run dev`
+
+## **Setup Guide**
+
+    Make a .env file.
+    Search [google gemini api.](https://aistudio.google.com/)
+    Click on Go To Ai Studio.
+    Click on get code and copy the code and paste in config file.
+    Click on Get API Keys.
+    Create a Api key.
+    Select Gemini Api.
+    Generate Api key and Copy that key.
+    Copy the Key and paste in .env file.
+    Install the node modules with npm i.
+    Run the application using npm start.
+
+## **Contribute**
+
+    This project is still in development and has a lot of exciting features to be added. **Contributions are welcome and encouraged!** Whether you're fixing bugs, improving documentation, or adding new features, your help is appreciated.
+
+    Drop a ⭐ on the Github repository (optional)
+
+    Create an issue of the project or a feature you would like to add in the project and get the task assigned to you.(Issue can be any bug fixes or any feature you want to add in this project).
+
+    Fork the repo to your Github.
+
+    Clone the Repo by going to your local Git Client in a particular local folder in your local machine by using this command with your forked repository link in place of below given link:
+   `git clone https://github.com/UrsTrinadh/Gemini-Clone.git`
+  `git branch <your branch name>`
+- Checkout to your branch.
+  `git checkout <your branch name>`
+- Add your code in your local machine folder.
+  `git add . `
+- Commit your changes.
+  `git commit -m"<add your message here>"`
+- Push your changes.
+  `git push --set-upstream origin <your branch name>`
+
+- Make a pull request! (compare your branch with the owner main branch)
+
+    Show some ❤️ by starring this awesome repository!
+
+   
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,89 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/context.jsx'
+const Main = () => {
+
+    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Google Gemini</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+
+                {!showResult
+                ? <>
+                <div className="greet">
+                    <p><span>Hey, Trinadh </span></p>
+                    <p>How can i assist you?</p>
+                </div>
+                <div onClick={()=>alert("Sorry, the random question feature is not available yet. I'm working on it. Thanks for your patience! - Trinadh")} className="cards">
+                    <div className="card">
+                        <p>Suggest some beautiful places to see on my upcoming roadtrips</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Breifly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstrom team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card excep">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                </>
+                :<div className='result'>
+                    <div className="result-title">
+                        <img  src={assets.user_icon} alt="" />
+                        <p>{recentPrompt}</p>
+                    </div>
+                    <div className="result-data">
+                        <img  src={assets.gemini_icon} alt="" />
+                        {loading
+                        ?
+                    <>
+                    <div className="loader">
+                            <hr />
+                            <hr />
+                            <hr />
+                    </div>
+                    </>: ( <p dangerouslySetInnerHTML={{__html:resultData}}></p> )
+                    }
+                       
+                    </div>
+                </div>
+                }
+
+                
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter your prompt...' />
+                        <div>
+                            <img onClick={()=>alert("Sorry, the video and audio features are not available yet. I'm working on making on it. Thanks for your patience! - Trinadh")} src={assets.gallery_icon} alt="" />
+                            <img onClick={()=>alert("Sorry, the video and audio features are not available yet. I'm working on making on it. Thanks for your patience! - Trinadh")} src={assets.mic_icon} alt="" />
+                           {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null} 
+                        </div>
+                    </div>
+                <p className="bottom-info">Google Gemini may provide incorrect info, including about people so double-check its responses.Your privacy and Gemini Apps</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
+
+
+
+
+
+
+
+
